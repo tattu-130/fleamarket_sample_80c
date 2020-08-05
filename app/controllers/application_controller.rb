2ecb7916d94c127caf4b5e7bdc72ecc7eeac2e9a
kosaito222 <kosaito222@gmail.com>
2020-08-06 02:31:49 +0900

    Update Session User
    # ログイン、ログアウト機能の実装
    - トップページにログイン、ログアウト、新規登録ボタンを実装
    - ユーザーのログイン状態を見てボタンの表示を切替

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  before_action :authenticate_user!
+  # ログインしていない場合にログインページへ遷移
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -15,16 +16,20 @@ class ApplicationController < ActionController::Base
       password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
-
+  
   protected
-
+  
+  # ユーザー新規登録時のストロングパラメーター
   def configure_permitted_parameters
-    # paramsにbirthdayを年月日に分けて追加
-    birthday = params[:birthday]
-    params["user"]["birth_year"] = birthday["birth_year(1i)"]
-    params["user"]["birth_month"] = birthday["birth_year(2i)"]
-    params["user"]["birth_day"] = birthday["birth_year(3i)"]
 
+    # paramsにbirthdayを年月日に分けて追加
+    if params[:birthday].present?
+      birthday = params[:birthday]
+      params["user"]["birth_year"] = birthday["birth_year(1i)"]
+      params["user"]["birth_month"] = birthday["birth_year(2i)"]
+      params["user"]["birth_day"] = birthday["birth_year(3i)"]
+    end
+    
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_year, :birth_month, :birth_day])
   end
   
