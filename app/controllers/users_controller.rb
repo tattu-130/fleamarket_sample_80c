34656f251e257f481c5812f3bfb27ea82e4e760b
kosaito222 <kosaito222@gmail.com>
2020-08-07 21:53:54 +0900

    [add]配送先情報の登録および表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @nickname = current_user.nickname
+    user = User.find(current_user.id)
+    @user_des = user.destination
   end
 end
