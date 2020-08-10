f38342c4cc9c59d5a2c356ed37b9f52ac1f954ec
kosaito222 <kosaito222@gmail.com>
2020-08-11 01:02:53 +0900

    [add]商品出品機能
    - 画像を複数枚選んで投稿可能
    - 投稿した商品は商品一覧（index）ページに反映
    - 商品の一覧はrenderで読み込む

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+console.log("ok");
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,20 +2,23 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :destroy]
 
   def index
-    @items = Item.all
+    @items = Item.includes(:item_imgs).order('created_at DESC')
   end
   
 
   def new
+    @item = Item.new
+    @item.item_imgs.new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
+    @item = Item.new(item_params)
+    if @item.save!
+      redirect_to root_path
+    else
+      render :new
+      puts "失敗"
+    end
   end
 
   def destroy
@@ -26,10 +29,14 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
-
   def set_item
     @item = Item.find(params[:id])
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, item_imgs_attributes: [:src]).merge(:user_id => current_user.id)
+  end
+
 end
