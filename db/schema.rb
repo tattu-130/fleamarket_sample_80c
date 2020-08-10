3b13b70f5fd41810122e9d37f2a330d517f82362
kosaito222 <kosaito222@gmail.com>
2020-08-10 19:25:55 +0900

    [fix]item_imgsテーブルのカラム名をurlからsrcに変更

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 2020_08_07_113042) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_08_10_102325) do
 
   create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "destination_first_name", null: false
@@ -18,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_113042) do
   end
 
   create_table "item_imgs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "url"
+    t.string "src"
     t.bigint "item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
