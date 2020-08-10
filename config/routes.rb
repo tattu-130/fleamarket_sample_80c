554a740f3146ee8cf2aecf01d39550e51668c0d3
kosaito222 <kosaito222@gmail.com>
2020-08-10 18:00:51 +0900

    [add]出品画面へのルーティング実装
    ## ログイン時
    - ユーザーの出品画面へ遷移
    
    ## 未ログイン時
    - ログイン画面へ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items
+  resources :items, except: :new
   resources :users, only: :show do
     resources :destinations, only: [:new, :create, :destroy]
+    resources :items, only: :new
   end
 end
