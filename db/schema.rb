73810705445387c19d22dac6634d5961d42ec8ac
kosaito222 <kosaito222@gmail.com>
2020-08-11 14:20:23 +0900

    [add]itemsテーブルにカラムの追加
    ## sellerカラム
    - 出品時に必ずフラグを立てる。
    - 出品されたかどうかを管理
    
    ## buyerカラム
    - 売れたときにフラグを立てる。
    - 本フラグが立っている商品に対して「売れた」処理をする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_102325) do
+ActiveRecord::Schema.define(version: 2020_08_11_051541) do
 
   create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "destination_first_name", null: false
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2020_08_10_102325) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.boolean "seller", default: true, null: false
+    t.boolean "buyer", default: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
