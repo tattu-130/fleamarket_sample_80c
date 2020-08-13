a8816b7750637d0d940c6afed9b3b8a3523ca498
Ishikawa-Ishiko <ishiko.9301@gmail.com>
2020-08-13 15:26:03 +0900

    [fix]Categoryのview調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :user, 
+  belongs_to :user
   belongs_to :category
   has_many :item_imgs, dependent: :destroy
   accepts_nested_attributes_for :item_imgs, allow_destroy: true
