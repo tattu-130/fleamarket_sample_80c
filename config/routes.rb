e00dbd9fd8a05a034614298e8ba30340d6c69100
Ishikawa-Ishiko <ishiko.9301@gmail.com>
2020-08-17 14:33:07 +0900

    [add]出品・編集にカテゴリを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, except: :new
+  resources :items, except: :new do
+    member do
+      get 'category_children', defaults:{ format: 'json'}
+      get 'category_grandchildren', defaults:{ format: 'json'}
+    end
+  end
   resources :users, only: :show do
     resources :destinations, only: [:new, :create, :destroy]
-    resources :items, only: :new
+    resources :items, only: :new 
   end
 end
