3ec62e5cbea95240078418c0e955d35245b869a4
tattu-130 <68007173+tattu-130@users.noreply.github.com>
2020-08-18 19:02:01 +0900

    編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-
+  before_action :set_parents, only: [:show]
   def show
     @nickname = current_user.nickname
     @image = current_user.user_image
     @user_des = current_user.destination
+  end
+
+  def set_parents
+    @parents = Category.where(ancestry: nil)
   end
 end
