d9edcf84c91be2cfa77063e29c8068685636fda5
gitsuguru <sugusugu.ryouri@gmail.com>
2020-08-21 12:00:58 +0900

    TextCount
    [add]文字カウントの数え方を降順に実装
    
    文字カウントを降順にし残り何文字かわかりやすくした。また10文字以下になると、文字色が赤色になるようにも実装

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,22 @@
 $(function(){
   // 自己紹介の文字数を数える
+  let text_max = 300;
+
   $(".FormField__input--width").on('keyup', function(){
-    $(".count").text($(this).val().length + '文字');
+    $(".count_number").text(text_max - $(this).val().length + '文字');
+
+    // ifで10以下なら文字の色分け
+    let text_length = $(this).val().length
+    let countdown = text_max - text_length
+    if(countdown <= 10){
+      $('.count_number').css({
+        color:'#ff0000',
+      });
+    } else {
+      $('.count_number').css({
+          color:'#000000',
+          fontWeight:'normal'
+      });
+    }
   });
 });
