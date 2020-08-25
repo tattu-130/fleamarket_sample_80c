6f153c2af34883913dded90dc0c4cd60b0235913
tattu-130 <68007173+tattu-130@users.noreply.github.com>
2020-08-25 13:16:00 +0900

    テストコード

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+  describe '#create' do
+    it "user_idとcard_idとustomer_idがあれば登録できること" do
+      user = create(:user)
+      card = build(:card)
+      expect(card).to be_valid
+    end
+    it "userがいなければ登録できないこと" do
+      user = create(:user)
+      card = build(:card, user_id: "")
+      card.valid?
+      expect(card.errors[:user_id]).to include("を入力してください")
+    end
+    it "customer_idがなければ登録できないこと" do
+      user = create(:user)
+      card = build(:card, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+    it "card_idがなければ登録できないこと" do
+      user = create(:user)
+      card = build(:card, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
+  end
 end
